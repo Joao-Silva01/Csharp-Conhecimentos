551048f610fa11d370e8af0c9f9279da1dd01b0a
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2023-07-07 16:09:14 -0300

    Novos métodos

--- Métodos_Array.cs
+++ Métodos_Array.cs
@@ -2,14 +2,16 @@
 
 public class Métodos_Array
 {
-    /*static void Main()
+    static void Main()
     {
+        Array_Mtds teste = new Array_Mtds();
+        teste.Array_SR();
         Console.ReadLine();
-    }*/
+    }
 }
 public class Array_Mtds
 {
-    public void Arr_ay()
+    public void Array_Copy()
     {
         string[] Vetor_Principal = new string[3] { "Arroz", "Feijão", "Frango" };
         string[] Vetor_Secundario = new string[3];
@@ -23,6 +25,47 @@ public class Array_Mtds
         foreach (string p in Vetor_Secundario)
         {
             Console.WriteLine(p);
+        }
+    }
+    public void Array_get()
+    {
+        int[] Numero = new int[5] { 5, 3, 2, 6, 4 };
+        Console.WriteLine("Maior índice: "+ Numero.GetUpperBound(0));
+        Console.WriteLine("Menor índice: "+ Numero.GetLowerBound(0));
+        Console.WriteLine("Quantos valores tem: "+ Numero.GetLongLength(0));
+        Console.WriteLine("Valor do índice 3: " + Convert.ToInt32(Numero.GetValue(3)));
+    }
+    public void Array_I()
+    {
+        int[] Num = new int[6] { 5, 3, 2, 6, 4, 3};
+        Console.WriteLine("Primeira posição do 3 no Array: {0} posição",Array.IndexOf(Num, 3));
+        Console.WriteLine("Primeira posição do 3 no Array: {0} posição",Array.LastIndexOf(Num,3));
+    }
+    public void Array_SR()
+    {
+        int[] Num = new int[6] { 6, 1, 4, 5, 2, 3 };
+        Console.WriteLine("Array Original:");
+        foreach (int n in Num)
+        {
+            Console.Write("{0} |", n);
+        }
+        Array.Sort(Num);
+        Console.WriteLine("\nArray Crescente:");
+        foreach (int n in Num)
+        {
+            Console.Write("{0} |",n);
+        }
+        Console.WriteLine("\nArray Decrescente:");
+        Array.Reverse(Num);
+        foreach (int n in Num)
+        {
+            Console.Write("{0} |", n);
+        }
+        Num.SetValue(10, 2);
+        Console.WriteLine("\nAdicionando Valor 10 na posição 2:");
+        foreach (int n in Num)
+        {
+            Console.Write("{0} |", n);
         }
     }
 }
